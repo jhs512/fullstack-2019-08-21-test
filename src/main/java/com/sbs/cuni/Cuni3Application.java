f31cc8cdefafe8ab830a5a65581e1902f19d3fb4
jhs512 <jangka512@gmail.com>
2019-08-22 11:39:56 +0900

    처음 커밋

---
+++ src/main/java/com/sbs/cuni/Cuni3Application.java
@@ -0,0 +1,13 @@
+package com.sbs.cuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cuni3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cuni3Application.class, args);
+	}
+
+}
